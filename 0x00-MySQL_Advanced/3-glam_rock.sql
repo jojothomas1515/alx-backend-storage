3e66d0eff67aa746c2670777a2e2a50e0481fb1f
Jojo <jojothomas1515@gmail.com>
2023-08-10 10:18:09 +0100

    bands with glam rock style lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- select bands with Glam rock as the main styles and calculate the lifspan with info or form data and split date
 SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
-WHERE style Like 'Glam rock%'
+WHERE style Like '%Glam rock%'
 ORDER BY lifespan DESC;
