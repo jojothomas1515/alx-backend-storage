37adac72f860cb2fd17725fdbd4c50b175ab0993
Jojo <jojothomas1515@gmail.com>
2023-08-11 09:45:54 +0100

    avg

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,10 +6,10 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser//
 -- start procedure
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-DECLARE av INT;
+DECLARE av FLOAT;
 DECLARE uids INT DEFAULT user_id;
-SET av = (SELECT AVG(score) FROM corrections as c WHERE c.user_id = user_id);
-UPDATE users SET average_score = av WHERE id = user_id;
+SET av = (SELECT AVG(score) FROM corrections as c WHERE c.user_id = uids);
+UPDATE users SET average_score = av WHERE id = uids;
 END; //
 
 -- revert delimiter
