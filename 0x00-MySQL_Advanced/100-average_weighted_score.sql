8a9b1c173fbbb190eeb0e0c8e74d72e92a47ea5f
Jojo <jojothomas1515@gmail.com>
2023-08-11 14:47:44 +0100

    advance procedure

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,21 @@
+-- procedure
+-- change from default delimiter
+delimiter //
+
+-- procedure
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+DECLARE w_d INT;
+DECLARE wa_vg INT;
+SET w_d = (SELECT SUM(weight) FROM projects);
+SELECT SUM((c.score * (p.weight / w_d))) INTO wa_vg
+  FROM corrections AS c
+  JOIN projects AS p
+  ON c.project_id = p.id
+  WHERE c.user_id = user_id;
+UPDATE users SET average_score = wa_vg
+  WHERE id = user_id;
+END; //
+
+-- change delimiter back to default
+delimiter ;
