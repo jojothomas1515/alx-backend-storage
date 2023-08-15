c887985be1bd4622cbfcc7f31a1be7d92b8cdb6c
Jojo <jojothomas1515@gmail.com>
2023-08-15 20:57:11 +0100

    update topic

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,10 +3,9 @@
 update documents.
 """
 
-from pymongo.collection import Collection
 
-def update_topics(mongo_collection: Collection, name, topics):
+def update_topics(mongo_collection, name, topics):
     """
     update topics
     """
-    mongo_collection.update_many({'name':name}, {'$set': {'topics': topics}})
+    mongo_collection.update_many({'name': name}, {'$set': {'topics': topics}})
