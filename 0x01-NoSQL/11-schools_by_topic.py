6775780eda1c9c2ae7ffc79881f8b51f51709a57
Jojo <jojothomas1515@gmail.com>
2023-08-15 21:02:18 +0100

    search topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+filter by topics.
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    filter the function by topic.
+    """
+    return mongo_collection.find({"topic": topic})
