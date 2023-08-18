17603d88f380529d13177deba776f51afff371f3
Jojo <jojothomas1515@gmail.com>
2023-08-18 20:51:51 +0100

    excersize

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,14 +12,14 @@ class Cache:
 
     def __init__(self):
         """Constructor."""
-        self._redis = redis.Redis()
+        self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[bytes, str, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """generate a random key (e.g. using uuid).
         store the input data in Redis using the
         random key and return the key.
         """
-        key = str(uuid4())
+        key: str = str(uuid4())
         self._redis.set(key, data)
         return key
