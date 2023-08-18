960c19534737aa1f154f40278c77e2db7f6ca70e
Jojo <jojothomas1515@gmail.com>
2023-08-18 20:46:04 +0100

    excersize

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Excercise."""
+import redis
+from typing import Union
+from uuid import uuid4
+
+
+class Cache:
+    """
+    Redis cache class.
+    """
+
+    def __init__(self):
+        """Constructor."""
+        self._redis = redis.Redis()
+        self._redis.flushdb
+
+    def store(self, data: Union[bytes, str, int, float]):
+        """generate a random key (e.g. using uuid).
+        store the input data in Redis using the
+        random key and return the key.
+        """
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
