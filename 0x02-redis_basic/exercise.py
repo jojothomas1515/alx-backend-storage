7994d3caa00a9f4aace1c7c0427e49851ef7206f
Jojo <jojothomas1515@gmail.com>
2023-08-18 21:36:55 +0100

    cache get method converting from bytes to whatever using the function you passed as an argument

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Excercise."""
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 from uuid import uuid4
 
 
@@ -16,10 +16,28 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """generate a random key (e.g. using uuid).
+        """
+        Generate a random key (e.g. using uuid).
+
         store the input data in Redis using the
         random key and return the key.
         """
         key: str = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable])\
+            -> Union[str, bytes, int, float]:
+        """Get value with reference to a key."""
+        val: Union[bytes, None] = self._redis.get(key)
+        if callable(fn):
+            return fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        """Call the get method with a str coversion method."""
+        return self.get(key, lambda x: str(x))
+
+    def get_int(self, key: int) -> int:
+        """Call the get method with a str coversion method."""
+        return self.get(key, lambda x: int(x))
